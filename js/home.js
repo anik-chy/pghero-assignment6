93b7728db04c5da2164111d5f622184dad2f2a87
anik-chy <anikchy2140@gmail.com>
2023-03-03 20:29:22 +0600

    card features added

--- js/home.js
+++ js/home.js
@@ -8,24 +8,32 @@ fetch("https://openapi.programming-hero.com/api/ai/tools")
   .then((data) => displayAiCards(data))
   .catch((error) => console.log(error));
 
+// extract features and make list
+function extractFeList(card){
+    return card.features.reduce((previous,current)=>{
+        return previous+'<li>'+current+'</li>';
+    },'');
+}
 // display card data
 const displayAiCards = (data) => {
   const cardContainer = document.getElementById("card-container");
   // cardContainer.textContent = '';
 
   //display 6 cards
-  cards = data.data.tools.slice(0, 6);
+//   cards = data.data.tools.slice(0, 6);
+    cards = data.data.tools;
 
   cards.forEach(card => {
-    cl("anik");
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("col");
+    featureStr = extractFeList(card);
+    cl(featureStr);
     cardDiv.innerHTML = `
         <div class="card h-100">
             <img src="${card.image}" class="card-img-top">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">Features</h5>
+                <ol>${featureStr}</ol>
             </div>
             <div class="card-footer">
                 <small class="text-muted">Last updated 3 mins ago</small>
