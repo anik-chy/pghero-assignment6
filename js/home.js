4598ff9cbd1e1bfe194045e69bcae951f253a6eb
anik-chy <anikchy2140@gmail.com>
2023-03-03 20:02:42 +0600

    cards skeleton added

--- js/home.js
+++ js/home.js
@@ -1,5 +1,37 @@
+// debuging function
+function cl(anything) {
+  console.log(anything);
+}
+// fetch the whole list
 fetch("https://openapi.programming-hero.com/api/ai/tools")
   .then((res) => res.json())
-  .then((data) => console.log(data))
+  .then((data) => displayAiCards(data))
   .catch((error) => console.log(error));
 
+// display card data
+const displayAiCards = (data) => {
+  const cardContainer = document.getElementById("card-container");
+  // cardContainer.textContent = '';
+
+  //display 6 cards
+  cards = data.data.tools.slice(0, 6);
+
+  cards.forEach(card => {
+    cl(card);
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("col");
+    cardDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+          `;
+    cardContainer.appendChild(cardDiv); 
+  });
+};
