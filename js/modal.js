ec44905a340e6f0cf50c21e4a796884c48d2acec
anik-chy <anikchy2140@gmail.com>
2023-03-04 01:33:58 +0600

    modal completed

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 function showModal(id) {
-  if (id < 10) {
+  cl(id)
+    if (id < 10) {
     id = "0" + id;
   }
   const urlStr = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -9,48 +10,85 @@ function showModal(id) {
     .catch((error) => console.log(error));
 }
 
+// extract modal integrations and make list
+function extractModalInList(data) {
+    const tempIntegration = data.data.integrations;
+    let integrationString = '';
+    if(!tempIntegration)
+    {
+        return 'No data Found';
+    }
+    else{
+        return tempIntegration.reduce((previous, current) => {
+            return previous + '<li>' + current + '</li>';
+          }, "");
+
+    }
+  }
+
+// extract modal features and make list
+function extractModalFeList(data) {
+  const tempFeature = data.data.features;
+  let featureString = '';
+  for (const key in tempFeature) {
+    if (Object.hasOwnProperty.call(tempFeature, key)) {
+        featureString += "<li>" + tempFeature[key].feature_name + "</li>";
+    }
+  }
+  return featureString;
+}
+
 function generateModal(data) {
-  const modalContainer = document.getElementById("modal-container");
-  modalContainer.textContent = "";
+  //preprocessed data
+  const modalFeaturedList = extractModalFeList(data);
+  const integrationsList = extractModalInList(data);
 
-  const modalBody1 = document.createElement("div");
-  modalBody1.classList.add("col");
+  
+    const modalContainer = document.getElementById("modal-container");
+    modalContainer.textContent = "";
 
-  modalBody1.innerHTML = `
-    <div class="card">
-        <div class="card-body bg-info">
-            <h5 class="card-title">${data.data.description}</h5>
-            <div class="d-flex text-center justify-content-center">
-                <p class="btn-light p-3 m-2 rounded">10/month<br>Basic</p>
-                <p class="btn-light p-3 m-2 rounded">10/month<br>Basic</p>
-                <p class="btn-light p-3 m-2 rounded">10/month<br>Basic</p>
-            </div>
-            <div class="d-flex justify-content-between">
-                <div>
-                    <h5>Features</h5>
-                    <ul></ul>
-                </div>
-                <div>
-                    <h5>Integrations</h5>
-                    <ul></ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
-  modalContainer.appendChild(modalBody1);
+    const modalBody1 = document.createElement("div");
+    modalBody1.classList.add("col");
 
-  const modalBody2 = document.createElement("div");
-  modalBody2.classList.add("col");
-
-  modalBody2.innerHTML = `
+    modalBody1.innerHTML = `
       <div class="card">
-          <img src="${data.data.image_link[0]}" class="card-img-top">
-          <div class="card-body text-center">
-              <h5 class="card-title">${data.data.input_output_examples[0].input}</h5>
-              <p class="card-text">${data.data.input_output_examples[0].output}</p>
+          <div class="card-body bg-info">
+              <h5 class="card-title">${data.data.description}</h5>
+              <div class="d-flex text-center justify-content-center">
+                  <p class="btn-light p-3 m-2 text-primary rounded">${data.data.pricing ? data.data.pricing[0].price : 'Free of Cost'}<br>${data.data.pricing ? data.data.pricing[0].plan : 'Basic'}</p>
+                  <p class="btn-light p-3 m-2 text-warning rounded">${data.data.pricing ? data.data.pricing[1].price : 'Free of Cost'}<br>${data.data.pricing ? data.data.pricing[1].plan : 'Pro'}</p>
+                  <p class="btn-light p-3 m-2 text-danger rounded">${data.data.pricing ? data.data.pricing[2].price : 'Free of Cost'}<br>${data.data.pricing ? data.data.pricing[2].plan : 'Enterprise'}</p>
+              </div>
+              <div class="d-flex justify-content-between">
+                  <div>
+                      <h5>Features</h5>
+                      <ul>${modalFeaturedList}</ul>
+                  </div>
+                  <div>
+                      <h5>Integrations</h5>
+                      <ul>${integrationsList}</ul>
+                  </div>
+              </div>
           </div>
       </div>
       `;
-  modalContainer.appendChild(modalBody2);
+    modalContainer.appendChild(modalBody1);
+
+    const modalBody2 = document.createElement("div");
+    modalBody2.classList.add("col");
+
+    modalBody2.innerHTML = `
+        <div class="card position-relative">
+            <img src="${data.data.image_link[0]}" class="card-img-top h-50">
+            <div class="position-absolute top-0 end-0 m-2 p-1 bg-danger">hiHello</div>
+            <div class="card-body text-center">
+                <h5 class="card-title">${data.data.input_output_examples ? data.data.input_output_examples[0].input : 'Can you give any example?'}</h5> 
+                <p class="card-text">${data.data.input_output_examples ? data.data.input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
+            </div>
+        </div>
+        `;
+    modalContainer.appendChild(modalBody2);
 }
+
+// 
+// 
